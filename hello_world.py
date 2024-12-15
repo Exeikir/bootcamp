250d8ad0356523ca6b31f8ecc6dea9aa4f03e123
Exeikir <alexei.kirkham@gmail.com>
2024-12-15 14:06:23 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# request input from user "What is your name?: "
+# store input as variable "name"
+# output "name"
+name = input("What is your name?: ")
+print("Your name is {}.".format(name))
+
+# request input from user "What is your age?:"
+# store input as variable "age"
+# output "age"
+age = input("What is your age?: ")
+print("Your age is {}.".format(age))
+
+# Print output "Hello World!"
+print("Hello World!")
