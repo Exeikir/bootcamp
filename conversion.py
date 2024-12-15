e69181199a4f9412e524c824ce391e92ca73cb58
Exeikir <alexei.kirkham@gmail.com>
2024-12-15 14:32:22 +0000

    Task 2

---
+++ conversion.py
@@ -0,0 +1,23 @@
+# Declare variables num1, num2, num3, string1
+# Use casting to:
+# - convert num1 into an interger called num1_int with int()
+# - convert num2 into a float called num2_float with float()
+# - convert num3 into a string called num3_str with str()
+# - convert sting1 into an interger called string1_int with int()
+# print all variables on seperate lines
+
+num1 = 99.23
+num1_int = int(num1)
+print(num1_int)
+
+num2 = 23
+num2_float = float(num2)
+print(num2_float)
+
+num3 = 150
+num3_str = str(num3)
+print(num3_str)
+
+string1 = "100"
+string1_int = int(string1)
+print(string1_int)
